39d3e5316885cd34ca4dd6eb5f386ba9f7a64768
Brandon Dang <brandondang100@gmail.com>
2019-01-24 10:46:58 -0800

    added Javascript!

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,25 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript is eating pizza");
+		$("#testjs").text("Please wait...");
+		$("#testjs").addClass("active");
+		$("testjs").toggleClass("active");
 	});
-
+function projectClick(e) {	
+$("a.thumbnail").click(projectClick);
+	 e.preventDefault();
+	 $(this).css("background-color", "#7fff00");
+	 var containingProject = $(this).closest(".project");
+     var description = $(containingProject).find(".project-description");
+     if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
+}
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+
+
 }
